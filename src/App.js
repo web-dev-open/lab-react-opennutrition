c75ff6fd16de4332dae4ab18f22ae8e549c275cb
Mohammed Irfan <irfan.arshad4@gmail.com>
2022-10-17 17:34:25 +0530

    Iteration-1 with and ant-D single col-row format complete

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-import { Card, Row, Col, Divider, Input, Button } from  'antd';
-import foods from  './foods.json';
+import logo from "./logo.svg";
+import "./App.css";
+import { Card, Row, Col, Divider, Input, Button } from "antd";
+import foods from "./foods.json";
 
 function App() {
   return (
     <div className="App">
-
+      <h1 className=" mt-10 font-bold text-lg">Food List</h1>
+      {/* <Row>
+        <Col span={12}>col-12</Col>
+        <Col span={12}>col-12</Col>
+      </Row> */}
+      {foods.map((foodItem, index) => {
+        return (
+          <>
+            <Row>
+              <Col span={24}>
+              <div className="flex-col self-start p-5">
+              <p className="p-5 flex self-start"> {foodItem.name} </p>
+              <img src={foodItem.image} width={120} />
+            </div>
+              </Col>
+            </Row>
+      
+          </>
+        );
+      })}
+      <div></div>
     </div>
   );
 }

3fab98c75e2fb6fb214691afeaba51bac8d5a813
Armando Costa <83301015+Costa0910@users.noreply.github.com>
2023-11-21 17:34:29 +0000

    done

--- src/App.js
+++ src/App.js
@@ -5,28 +5,54 @@ import foods from "./foods.json";
 
 // Components
 import FoodBox from "./FoodBox";
+import AddFoodForm from "./AddFoodForm";
+import SearchBar from "./SearchBar";
 
 //Ant Design
-import { Row } from "antd";
+import { Row, Divider, Button } from "antd";
+
+// React
+import { useState } from "react";
 
 function App() {
+  const [state, setState] = useState({
+    foods: foods,
+    filteredFoods: foods,
+    addNewFood: false,
+  });
   return (
     <div className="App">
-      <h1>Hello World</h1>
-      <section className="foods">
-        <Row
-        // gutter={{
-        //   xs: 8,
-        //   sm: 16,
-        //   md: 24,
-        //   lg: 32,
-        // }}
+      <h1>Food Tracker</h1>
+      <Divider>Add Food Entry</Divider>
+      {state.addNewFood ? (
+        <>
+          <AddFoodForm addFood={setState} />
+          <Button
+            style={{ marginTop: 10 }}
+            type="primary"
+            onClick={() => setState({ ...state, addNewFood: false })}
+          >
+            Hide Form
+          </Button>
+        </>
+      ) : (
+        <Button
+          style={{ marginTop: 10 }}
+          type="primary"
+          onClick={() => setState({ ...state, addNewFood: true })}
         >
-          {foods.map((food) => (
-            <FoodBox key={food.name} food={food} />
-          ))}
-        </Row>
-      </section>
+          Add New Food
+        </Button>
+      )}
+      <Divider>Search</Divider>
+      <SearchBar updateFilteredFoods={setState} />
+      <Row style={{ width: "100%", justifyContent: "center" }}>
+        <Divider>Food List</Divider>
+        {state.filteredFoods.map((food) => (
+          <FoodBox key={food.name} food={food} deleteFood={setState} />
+        ))}
+      </Row>
+      {state.foods.length === 0 && <p>No foods to display</p>}
     </div>
   );
 }
--- src/FoodBox.js
+++ src/FoodBox.js
@@ -1,7 +1,19 @@
 import { Card, Col, Button } from "antd";
 
-function FoodBox({ food }) {
+function FoodBox({ food, deleteFood }) {
   const { name, image, calories, servings } = food;
+
+  function handleDelete() {
+    console.log(name);
+    deleteFood((prev) => {
+      const filteredFoods = prev.foods.filter((food) => food.name !== name);
+      return {
+        ...prev,
+        foods: filteredFoods,
+        filteredFoods: filteredFoods,
+      };
+    });
+  }
   return (
     <Col xs={12} md={8} lg={6}>
       <Card title={name} style={{ width: 230, height: 300, margin: 10 }}>
@@ -11,7 +23,10 @@ function FoodBox({ food }) {
         <p>
           <b>Total Calories: {calories * servings} </b> kcal
         </p>
-        <Button type="primary"> Delete </Button>
+        <Button type="primary" onClick={handleDelete} style={{ marginTop: 10 }}>
+          {" "}
+          Delete{" "}
+        </Button>
       </Card>
     </Col>
   );

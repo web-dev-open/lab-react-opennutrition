32b7a8b8e60b5d7b617884a9dd6dbde311c612c4
chathurahasanga44 <s92067130@ousl.lk>
2023-12-27 00:00:54 +0530

    done

--- src/index.js
+++ src/index.js
@@ -1,8 +1,60 @@
+// src/index.js
+import 'antd/dist/antd.css';
+import foods from './foods.json';
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+const [foodList, setFoodList] = useState(foods);
+
+return (
+  <div>
+    {foodList.map(food => (
+      <div key={food.name}>
+        <p>{food.name}</p>
+        <img src={food.image} width={100} alt={food.name} />
+      </div>
+    ))}
+  </div>
+);
+// src/FoodBox.js
+import { Card, Button } from 'antd';
+
+const FoodBox = ({ food }) => {
+  return (
+    <Card title={food.name}>
+      <img src={food.image} alt={food.name} />
+      <p>Calories: {food.calories}</p>
+      <p>Servings: {food.servings}</p>
+      <Button>Delete</Button>
+    </Card>
+  );
+};
+
+export default FoodBox;
+import FoodBox from './FoodBox';
+
+// Inside the App component
+{foodList.map(food => (
+  <FoodBox key={food.name} food={food} />
+))}
+// src/AddFoodForm.js
+import { Input, Button } from 'antd';
+
+const AddFoodForm = () => {
+  // Implement form logic here
+
+  return (
+    <div>
+      <Input placeholder="Name" />
+      {/* Add other input fields */}
+      <Button>Add Food</Button>
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(

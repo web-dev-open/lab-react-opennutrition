975f0a9f3c7ae98bf3053aafb84c2fe3327e49e4
M Sami <beboolean@gmail.com>
2024-01-25 22:36:40 +0530

    done

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+
 import App from './App';
+
+// src/index.js
+
+// import 'antd/dist/antd.css';
+
+
+
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
